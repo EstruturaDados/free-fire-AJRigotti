62eec7ad50191e0ed814ef73466cc045610f8697
Ana Julia Rigotti <anajuliarigottisoncin@gmail.com>
2025-10-20 23:19:36 +0000

    nivel novato

--- FreeFire.c
+++ FreeFire.c
@@ -19,16 +19,19 @@ int main(){
     //criando nossa mochila com 10 itens
     struct itens mochila[10];
 
-    //definindo uma variavel pra dizer quantos itens tem na mochila
-    int quantidade_itens = 0;
+    int quantidade_itens = 0; //definindo uma variavel pra dizer quantos itens tem na mochila
 
-    //variavel que armazena a opcao escolhida do menu
-    int opcao_menu;
+    int opcao_menu; //variavel que armazena a opcao escolhida do menu
+
+    int item_procurado; //variavel para procurar o item para ser removido
+
+    bool encontrado = false; //variavel para definir se o item foi encontrado ou nao
+
 
     //usando o do para exibir o menu varias vezes ate o usuario decidir sair
     do
     {
-        printf("Bem vindo!!\n");
+        printf("----------------Bem vindo!!----------------\n");
         printf("Escolha uma das opcoes abaixo para organizar sua mochila:\n");
         printf("\n");
 
@@ -39,8 +42,6 @@ int main(){
         printf("\n");
 
         printf("Digite uma opcao:\n");
-        scanf("%d", &opcao_menu);
-
 
         //para evitar que fique em loop infinito caso o usuario digite algo diferente de um numero
         if (scanf("%d", &opcao_menu) != 1) 
@@ -48,23 +49,24 @@ int main(){
         printf("Entrada invalida! Digite apenas numeros\n");
         printf("\n");
         while (getchar() != '\n'); //limpa o caractere do scanf
-        }
         continue;
+        }
+
 
 
         switch (opcao_menu)
     {
     case 1:
         
-        if (quantidade_itens <= 10)
+        if (quantidade_itens < 10)
         {
-            printf("Vamos adicionar um item!\n");
+            printf("----------------Vamos adicionar um item!----------------\n");
             printf("\n");
             printf("Qual sera o nome do seu item?\n");
-            scanf("%s", &mochila[quantidade_itens].nome_item);
+            scanf("%s", mochila[quantidade_itens].nome_item);
 
             printf("Qual é o tipo do seu item?\n");
-            scanf("%s", &mochila[quantidade_itens].tipo_item); 
+            scanf("%s", mochila[quantidade_itens].tipo_item); 
 
             printf("Qual o numero desse item?\n"); 
             scanf("%d", &mochila[quantidade_itens].numero_item); 
@@ -78,16 +80,64 @@ int main(){
             printf("Sua mochila esta cheia! Nao foi possivel adicionar o item\n");
         }
         
-        
-    
         break;
-    
+
     case 2:
 
+        printf("----------------Vamos remover um item!----------------\n");
+        printf("\n");
+
+        printf("Qual o numero do item que voce quer remover?\n");
+        if (scanf("%d", &item_procurado) != 1) //caso seja digitado algo diferente de numeros
+        {
+        printf("Entrada invalida. Voltando ao menu.\n");
+        break;
+        }
+
+        printf("Procurando o item...\n");
+        
+
+        for (int i = 0; i < quantidade_itens; i++)
+        {
+            if (mochila[i].numero_item == item_procurado)
+            {
+                mochila[i] = mochila[quantidade_itens - 1];
+                quantidade_itens--;
+                encontrado = true;
+
+                printf("Item removido!\n");
+            }
+
+        }
+        
+        if (encontrado == false)
+        {
+            printf("O item nao foi encontrado, portanto, nao foi removido.\n");
+        }
+
+        printf("\n");
+        
         break;
 
     case 3:
+        printf("----------------Lista dos seus itens:----------------");
+        printf("\n");
 
+        if (quantidade_itens == 0)
+        {
+            printf("Sua mochila esta vazia!\n");
+        } else {
+
+            for (int i = 0; i < quantidade_itens; i++)
+            {
+                printf("Item %d - Nome: %s \n", i + 1, mochila[i].nome_item);
+                printf("Item %d - Tipo: %s \n", i + 1, mochila[i].tipo_item);
+                printf("Item %d - Numero: %d \n", i + 1, mochila[i].numero_item);
+                printf("\n");
+            }
+            
+        }
+        
         break;
 
     case 4:
